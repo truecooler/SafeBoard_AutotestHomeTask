659c573f8077b06998b8e281e2a6171d78b67d15
TheCooler <truecooler@gmail.com>
2018-12-06 18:58:01 +0300

    Добавьте файлы проекта.

---
+++ SafeBoard_AutotestHomeTask/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SafeBoard_AutotestHomeTask
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

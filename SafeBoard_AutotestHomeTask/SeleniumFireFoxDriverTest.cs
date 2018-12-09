dcf72f87c8acc835c7a569de49e648ba09ce6640
TheCooler <truecooler@gmail.com>
2018-12-09 03:10:12 +0300

    поправил пару багов с селениумом, вынес драйвера в отдельную папку

--- SafeBoard_AutotestHomeTask/SeleniumFireFoxDriverTest.cs
+++ SafeBoard_AutotestHomeTask/SeleniumFireFoxDriverTest.cs
@@ -13,10 +13,22 @@ using Newtonsoft.Json;
 
 namespace SafeBoard_AutotestHomeTask
 {
+
+	//comfortable feature to execute js
+	public static class JavaScriptExecutor
+	{
+		public static IJavaScriptExecutor Scripts(this IWebDriver driver)
+		{
+			return (IJavaScriptExecutor)driver;
+		}
+	}
+
+
 	//[Ignore]
 	[TestClass]
 	public class SeleniumFireFoxDriverTest
 	{
+		public static string FireFoxDriversDirectory = "../../../../SeleniumFireFoxDrivers";
 		public class VkCredentials
 		{
 			public VkCredentials()
@@ -72,12 +84,15 @@ namespace SafeBoard_AutotestHomeTask
 			}
 		}
 
+
+		
+
 		//[Ignore]
 		[TestMethod]
 		//авторизоватся и оставить пост на своей стене
 		public void VK_LoginAndMakePost_ExceptionShouldNotOccur()
 		{
-			using (var driver = new FirefoxDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)))
+			using (IWebDriver driver = new FirefoxDriver(FireFoxDriversDirectory))
 			{
 				FixDriverCommandExecutionDelay(driver);
 				driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
@@ -88,11 +103,24 @@ namespace SafeBoard_AutotestHomeTask
 				driver.FindElement(By.Id("index_pass")).SendKeys(_VkCredentials.Password);//вставляем пароль
 				driver.FindElement(By.Id("index_login_button")).Click();//жмем кнопку входа
 				driver.FindElement(By.Id("l_pr")).Click();//открываем страницу нашего профиля
-				var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-				wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("post_field"))).SendKeys("hello from KasperskyLab!");//вводим текст в поле нового поста
-				//driver.FindElement(By.Id("send_post")).Click();//отправляем пост на стену
-				wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("send_post"))).Click();
+				
 
+				var input = driver.FindElement(By.Id("post_field"));
+				
+				driver.Scripts().ExecuteScript("arguments[0].scrollIntoView()", input);
+				input.SendKeys("умею быстро придумывать числа: " + new Random().Next(100000,9999999).ToString());//вводим текст в поле нового поста
+
+				//var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+				//wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("post_field"))).SendKeys("hello from KasperskyLab!");//вводим текст в поле нового поста
+				//driver.FindElement(By.Id("send_post")).Click();//отправляем пост на стену
+				//wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("send_post"))).Click();
+
+				var button = driver.FindElement(By.Id("send_post"));
+				driver.Scripts().ExecuteScript("arguments[0].click();", button);
+				//button.Click();
+
+
+				//отправляем пост на стену
 
 
 				Thread.Sleep(5000);
@@ -105,7 +133,7 @@ namespace SafeBoard_AutotestHomeTask
 		//авторизоваться, найти страницу Евгения, открыть его аватарку, поставить лайк и оставить комментарий
 		public void VK_LoginThenLikeAndCommentEugene_ExceptionShouldNotOccur()
 		{
-			using (var driver = new FirefoxDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)))
+			using (var driver = new FirefoxDriver(FireFoxDriversDirectory))
 			{
 				FixDriverCommandExecutionDelay(driver);
 				driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
@@ -122,7 +150,7 @@ namespace SafeBoard_AutotestHomeTask
 				driver.FindElement(By.XPath("//*[@id=\"friends_search_cont\"]/div[1]/div[1]/div[3]/div[1]/a")).Click();//жмем на первого человека в списке
 				driver.FindElement(By.XPath("//*[@id=\"profile_photo_link\"]/img")).Click();//открываем аватарку Евгения
 				driver.FindElement(By.XPath("//*[@id=\"pv_narrow\"]/div[1]/div[1]/div/div/div[1]/div[3]/div/div[1]/a[1]/div[1]")).Click();//ставим лайк на аватарку Евгению
-				driver.FindElement(By.XPath("//*[starts-with(@id,'reply_field')]")).SendKeys("желаю успехов");//набираем ему комментарий под фото
+				driver.FindElement(By.XPath("//*[starts-with(@id,'reply_field')]")).SendKeys("отличное фото");//набираем ему комментарий под фото
 				driver.FindElement(By.ClassName("addpost_button_wrap")).Click();//отправляем комментарий Евгению
 
 
@@ -136,7 +164,7 @@ namespace SafeBoard_AutotestHomeTask
 		//авторизоваться, открыть первого друга в списке друзей, открыть аватарку, поставить лайк, оставить комментарий
 		public void VK_LoginThenLikeAndCommentFirstFriend_ExceptionShouldNotOccur()
 		{
-			using (var driver = new FirefoxDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)))
+			using (var driver = new FirefoxDriver(FireFoxDriversDirectory))
 			{
 				FixDriverCommandExecutionDelay(driver);
 				driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
@@ -150,7 +178,7 @@ namespace SafeBoard_AutotestHomeTask
 				driver.FindElement(By.XPath("//*[@id=\"list_content\"]/div[1]/div[1]/div[4]/div[1]/a")).Click();//жмем на первого человека в списке
 				driver.FindElement(By.XPath("//*[@id=\"profile_photo_link\"]/img")).Click();//открываем аватарку пользователя
 				driver.FindElement(By.XPath("//*[@id=\"pv_narrow\"]/div[1]/div[1]/div/div/div[1]/div[3]/div/div[1]/a[1]/div[1]")).Click();//ставим лайк на аватарку
-				driver.FindElement(By.XPath("//*[starts-with(@id,'reply_field')]")).SendKeys("хорошо выглядишь");//набираем ему комментарий под фото
+				driver.FindElement(By.XPath("//*[starts-with(@id,'reply_field')]")).SendKeys("спасииибоооо");//набираем ему комментарий под фото
 				driver.FindElement(By.ClassName("addpost_button_wrap")).Click();//отправляем комментарий 
 
 
@@ -164,7 +192,7 @@ namespace SafeBoard_AutotestHomeTask
 		//авторизоваться, зайти в первую группу, зайти к случайному участнику, заблокировать его, и разблокировать
 		public void VK_LoginThenBlockAndUnlockUserFromFirstGroup_ExceptionShouldNotOccur()
 		{
-			using (var driver = new FirefoxDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)))
+			using (var driver = new FirefoxDriver(FireFoxDriversDirectory))
 			{
 				FixDriverCommandExecutionDelay(driver);
 				driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
@@ -178,10 +206,11 @@ namespace SafeBoard_AutotestHomeTask
 				driver.FindElement(By.XPath("//*[@id=\"groups_list_groups\"]/div[1]/a")).Click();//жмем на первую группу в списке
 				driver.FindElement(By.XPath("//*[contains(@id,'_followers')]/a/div")).Click();//открываем список участников
 				driver.FindElement(By.XPath("//*[@id=\"fans_rowsmembers\"]/div[1]/div[1]")).Click();//жмем на первого участника
-																									//Thread.Sleep(300);
 				var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
 				wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id=\"narrow_column\"]/div[1]/aside/div/div[2]/div[2]/div[1]"))).Click();//раскрываем меню дополнительных функций у пользователя
+				//Thread.Sleep(1000);
 				wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id=\"narrow_column\"]/div[1]/aside/div/div[2]/div[2]/div[2]/div[2]/a[4]"))).Click();//кликаем на пункт о блокировке пользователя
+				Thread.Sleep(2000);
 				wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id=\"narrow_column\"]/div[1]/aside/div/div[2]/div[2]/div[2]/div[2]/a[4]"))).Click();//кликаем на пункт о разблокировке пользователя
 
 				Thread.Sleep(5000);
